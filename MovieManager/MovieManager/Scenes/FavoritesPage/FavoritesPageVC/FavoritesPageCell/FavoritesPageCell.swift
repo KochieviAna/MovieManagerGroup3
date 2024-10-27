9ca0d709c5795bce4defd374f3cc24de682f3c51
Tariel Khalvashi <taro.khalvashi1225@gmail.com>
2024-10-27 12:33:35 +0400

    AddFavoritePage

--- MovieManager/MovieManager/Scenes/FavoritesPage/FavoritesPageVC/FavoritesPageCell/FavoritesPageCell.swift
+++ MovieManager/MovieManager/Scenes/FavoritesPage/FavoritesPageVC/FavoritesPageCell/FavoritesPageCell.swift
@@ -7,6 +7,83 @@
 
 import UIKit
 
-class FavoritesPageCell: UICollectionViewCell {
+final class FavoritesPageCell: UICollectionViewCell {
+    
+    static let identifier = "FavoritesPageCell"
+    
+    private let avatar: UIImageView = {
+        let imageOfFilm = UIImageView()
+        imageOfFilm.layer.cornerRadius = 10
+        imageOfFilm.layer.masksToBounds = true
+        imageOfFilm.layer.shadowColor = UIColor.black.cgColor
+        imageOfFilm.layer.shadowOpacity = 1.0
+        imageOfFilm.layer.shadowOffset = .zero
+        imageOfFilm.layer.shadowPath = UIBezierPath(roundedRect: imageOfFilm.bounds, cornerRadius: imageOfFilm.layer.cornerRadius).cgPath
+        imageOfFilm.layer.shadowRadius = 15
+        
+        return imageOfFilm
+    }()
+    
+    private let nameLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 20, weight: .medium)
+        label.textColor = .black
+        label.numberOfLines = 2
+        return label
+    }()
+    
+    private let ratingDescriptionLabel: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 18, weight: .light)
+        label.textColor = .systemGray
+        return label
+    }()
+    
+    private let favoriteImageStar: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(systemName: "star.fill")
+        imageView.tintColor = .systemYellow
+        return imageView
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupCell()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    private func setupCell() {
+        [avatar, nameLabel, ratingDescriptionLabel, favoriteImageStar].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            contentView.addSubview($0)
+        }
+     NSLayoutConstraint.activate([
+        avatar.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20),
+        avatar.centerXAnchor.constraint(equalTo: contentView.centerXAnchor),
+        avatar.heightAnchor.constraint(lessThanOrEqualToConstant: 200),
+        avatar.widthAnchor.constraint(lessThanOrEqualToConstant: 150),
+        
+        nameLabel.topAnchor.constraint(equalTo: avatar.bottomAnchor, constant: -500),
+        nameLabel.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+        nameLabel.centerYAnchor.constraint(equalTo: contentView.centerYAnchor, constant: 20),
+        nameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20),
+        
+        ratingDescriptionLabel.leftAnchor.constraint(equalTo: favoriteImageStar.leftAnchor, constant: 30),
+        ratingDescriptionLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: -30),
+        
+        favoriteImageStar.leftAnchor.constraint(equalTo: contentView.leftAnchor, constant: 20),
+        favoriteImageStar.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: -30)
+     ])
+    }
+    
+    func configure(movie: FilmModel) {
+        avatar.image = movie.image
+        nameLabel.text = movie.name
+        ratingDescriptionLabel.text = "\(movie.imdbRating)/10 IMDb"
+    }
     
 }
--- MovieManager/MovieManager/Scenes/FavoritesPage/FavoritesPageVC/FavoritesPageVC.swift
+++ MovieManager/MovieManager/Scenes/FavoritesPage/FavoritesPageVC/FavoritesPageVC.swift
@@ -8,8 +8,53 @@
 import UIKit
 
 final class FavoritesPageVC: UIViewController {
+    
+    private lazy var moviesCollectionView: UICollectionView = {
+        let collection: UICollectionView
+        let collectionLayout = UICollectionViewFlowLayout()
+        collectionLayout.scrollDirection = .vertical
+        collectionLayout.itemSize = CGSize(width: 180, height: 450)
+        collection = UICollectionView(frame: .zero, collectionViewLayout: collectionLayout)
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        
+        return collection
+    }()
+    let viewModel = FavoritesPageViewModel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        setupCollectionView()
+    }
+    
+    private func setupCollectionView() {
+        
+        view.addSubview(moviesCollectionView)
+        
+        moviesCollectionView.leftAnchor.constraint(equalTo: view.leftAnchor, constant: 0).isActive = true
+        moviesCollectionView.rightAnchor.constraint(equalTo: view.rightAnchor, constant: 0).isActive = true
+        moviesCollectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
+        moviesCollectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        moviesCollectionView.dataSource = self
+        moviesCollectionView.delegate = self
+        
+        moviesCollectionView.register(FavoritesPageCell.self, forCellWithReuseIdentifier: FavoritesPageCell.identifier)
     }
 }
+
+extension FavoritesPageVC: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        viewModel.numberOfFilms
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: FavoritesPageCell.identifier, for: indexPath) as? FavoritesPageCell else {
+            fatalError("Failed to dequeue CustomCollectionViewCell")
+        }
+
+        cell.configure(movie: viewModel.getFilm(at:indexPath.row))
+                       
+        return cell
+            
+        }
+    }
